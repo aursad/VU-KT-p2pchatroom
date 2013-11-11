085d1c354133cb250087e5dbbf4503abed82f7b2
Aurimas S <aurimas.sadauskas@gmail.com>
2013-11-12 00:04:18 +0200

    bandyta realizuoti multi zinuciu gavima, nepavyko, negaunama zinuciu

--- client/cnlabs_client_drv.c
+++ client/cnlabs_client_drv.c
@@ -1,103 +1,85 @@
-#include "cnlabs_client.h"
-int main(void)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+#include <unistd.h>
+
+#define MAXLINE 4096
+#define max(a,b)	((a) > (b) ? (a) : (b))
+
+void client_chat(FILE *fp, int sockfd)
 {
-    SOCKET ClientSockDesc;       // Kliento pagrindinio soketo-klausytojo deskriptorius.
-    char UserInput [2000] = {0}; // Masyvas vartotojo komandoms nuskaityti.
-    int SendResult;              // Siuntimo funkcijos resultatui saugoti.
-    int ReceiveResult;              // Siuntimo funkcijos resultatui saugoti.
-    char Packet [2000] = {0};    // Buferiu masyvas duomenims gauti.
-    int iCounter, jCounter;      // Skaitliukai.
-    unsigned int ParseResult;    // Komandu analizes rezultatui saugoti.
+	int 	maxfdp1, stdineof, i;
+	fd_set	rset;
+	char	buf[MAXLINE];
+	int 	n;
+	char    nickname[255];
+	char    lines[1024];
 
-        fd_set MainSocketSet, TempSet; //pagrindine soketu aibe; soketu aibe, kuri turi duomenu, paruostu nuskaitymui
-    unsigned int MaxKnownSocketDesc;// maksimalus deskriptoriu kintamasis (soketo nr), skaitliukas (soket¸)
-    struct timeval TimeVal; // Laiko struktura dirbti su select().
+	stdineof = 0;
+	FD_ZERO(&rset);
+	printf("what's your name?");
+    	scanf("%s",nickname);
+	for ( ; ; )
+	{
+		if (stdineof == 0)
+			FD_SET(fileno(fp), &rset);
+		FD_SET(sockfd, &rset);
+		maxfdp1 = max(fileno(fp), sockfd) + 1;
+		select(maxfdp1, &rset, NULL, NULL, NULL);
 
-    WSADATA wsaData;
-    if(WSAStartup(0x202, &wsaData) == 0)
-    {
-    }
-    else
-    {
-        printf("ERROR: Initialization failure.\n");
-    }
 
-    if ( INVALID_SOCKET == (ClientSockDesc = InitializeClient ()) )
-    {
-        printf ("CNLabs Client error: client initialization failed.\n");
-        goto EXIT;
-    }
-        // Inicializuoti soketu aibes.
-    FD_ZERO(&MainSocketSet);
-    FD_ZERO(&TempSet);
+		if (FD_ISSET(sockfd, &rset))
+		{
+			if ((n = read(sockfd, buf, MAXLINE)) == 0)
+			{
+				if (stdineof == 1)
+					return;
+				else
+				{
+					printf("client_chat: server terminated...\n");
+					exit(1);
+				}
+			}
+			write(fileno(stdout), buf, n);
+		}
 
-    // Inicializuojame laiko struktura. 0 - reiskia, kad select() funkcija turi blokuotis laukdama, kol atsiras bent vienas aktyvus soketas.
-    TimeVal.tv_sec = 0; //sekundes
-    TimeVal.tv_usec = 500; //tukstantosios
-        FD_SET(ClientSockDesc, &MainSocketSet);
-    MaxKnownSocketDesc = ClientSockDesc;
+		if (FD_ISSET(fileno(fp), &rset))
+		{
+			if ((n = read(fileno(fp), buf, MAXLINE)) == 0)
+			{
+				stdineof = 1;
+				shutdown(sockfd, SHUT_WR);
+				FD_CLR(fileno(fp), &rset);
+				continue;
+			}
+			write(sockfd, buf, n);
+		}
+	}
+}
 
-    while ( 1 )
-    {
-                // Kiekvienoje iteracijoje inicializuokime pagalbine soketu aibe pagrindine.
-        TempSet = MainSocketSet;
+int main(int argc, char **argv)
+{
+	int sockfd;
+	struct sockaddr_in servaddr;
 
-        // Isrenkame is soketu aibes tuos deskriptorius, kurie apraso soketus, turincius duomenu, paruostu skaitymui.
-        //select()tinklo primityvo panaudojimas sinchroniniu budu, kada blokuojamasi laukiant aktyviu soketu, turinciu info, kuria per juos galima skaityti is tinklo.
-        //tikrina zemesnius uz pirmuoji nr pazymetus, todel +1, pirmos eiles soketu aibe (2,3 eiles soketu aibe), blokavimas ribota laika
-        if ( SOCKET_ERROR == select (MaxKnownSocketDesc + 1, &TempSet, NULL, NULL, &TimeVal) )
-        {
-            exit(EXIT_FAILURE);
-        }
+	if (argc != 3)
+	{
+		printf("Usage: %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
 
-        // Nuskanuoti galimu deskriptoriu erdve iki turimo maksimalaus.
-        for ( iCounter = 0; iCounter <= MaxKnownSocketDesc; iCounter++ )
-        {
-            // Tikriname su kiekviena skaitliuko reiksme, ar jo aprasomas soketas nepriklauso pagrindinei soketu aibei.
-            if ( FD_ISSET (iCounter, &TempSet) )
-            {
-                // Jei tos skaitliuko reiksmes aprasomas soketas yra 'ServerSockDesc', t.y. nauju prisijungimu laukiantis, tai reikia apdoroti naujo kliento prisijungima.
-                //if ( iCounter == ClientSockDesc )
-                //{
-                    if ( SOCKET_ERROR == ReceivePacket (&ClientSockDesc, Packet) )
-                    {
-                        printf ("CNLabs Client error: data reception from server failed.\n");
-                        break;
-                    }
-                    UnmarshalPacket (Packet);
-                    printf ("%s\n", Packet);
-               //}
-            } else {
-                fflush ( stdin );
-                memset (UserInput, 0, sizeof (UserInput));
-                fgets (UserInput, sizeof (UserInput), stdin);
-                UserInput [strlen (UserInput) - 1] = '\0';
-                if ( 0 == strcmp (UserInput, "") ) continue;
-                ParseResult = ParseCommandInput (UserInput);
-                if ( 0 == ParseResult ) continue;
-                if ( 3 == ParseResult ) continue;
-                if ( 1 == ParseResult )
-                {
-                    if ( SOCKET_ERROR == (SendResult = SendPacket (&ClientSockDesc, UserInput, strlen (UserInput))) )
-                    {
-                        printf ("CNLabs Client error: data transmission to the server failed.\n");
-                        break;
-                    }
-                    if ( SOCKET_ERROR == ReceivePacket (&ClientSockDesc, Packet) )
-                    {
-                        printf ("CNLabs Client error: data reception from server failed.\n");
-                        break;
-                    }
-                    UnmarshalPacket (Packet);
-                    printf ("%s\n", Packet);
-                }
-                if ( 2 == ParseResult ) break;
-            }
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-        }
-    }
-    closesocket (ClientSockDesc);
-EXIT:
-    WSACleanup();
-    return 1;
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(atoi(argv[2]));
+	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+
+	connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+	client_chat(stdin, sockfd);
+
+	exit(0);
 }
